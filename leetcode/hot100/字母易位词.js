d7353842c07ba05b5780606fc172af27bbab6c4b
名字 <2801884530@qq.com>
2024-07-05 10:11:43 +0800

    update

--- leetcode/hot100/字母易位词.js
+++ leetcode/hot100/字母易位词.js
@@ -8,6 +8,8 @@ var groupAnagrams = function(strs) {
         //对str进行添加而不是arr,arr是字母异位词的判定
         map[arr] ? map[arr].push(str) : map[arr] = [str]
     }
+    //Object.values() 方法是在JavaScript中用于获取给定对象自身的所有可枚举属性值的数组。
+    //换句话说，该方法会提取对象中每个属性的值并将这些值作为数组中的元素返回。
     return Object.values(map)
 }
 let strs = ["eat","tea","tan","ate","nat","bat"]
