f1f2d61957629aaccc6f55400d80f2138a1a9ea4
名字 <2801884530@qq.com>
2024-07-05 15:00:51 +0800

    update

--- leetcode/hot100/3.无重复字符的最长子串.js
+++ leetcode/hot100/3.无重复字符的最长子串.js
@@ -1,13 +1,31 @@
 var lengthOfLongestSubstring = function (s) {
-    let arr = [], len = 0
-    for(let i = 0; i < s.length; i++){
-        if(!arr.includes(s[i])){
-            arr.push(s[i])
-        }else{
-            len = Math.max(len, arr.length)
-            while(arr.includes(s[i])) arr.shift()
-            arr.push(s[i])
-        }
-    }
-    return arr.length > len ? arr.length : len
-}
+	let arr = [],
+		len = 0;
+	for (let i = 0; i < s.length; i++) {
+		if (!arr.includes(s[i])) {
+			arr.push(s[i]);
+		} else {
+			len = Math.max(len, arr.length);
+			while (arr.includes(s[i])) arr.shift();
+			arr.push(s[i]);
+		}
+	}
+	return arr.length > len ? arr.length : len;
+};
+
+var lengthOfLongestSubstring2 = function (s) {
+	let str = "";
+	let len = 0;
+	for (let c of s) {
+		if (!str.includes(c)) {
+			str += c;
+			if (str.length > len) len = str.length;
+		} else {
+			str = str.substring(str.indexOf(c) + 1) + c;
+		}
+	}
+    return len
+}; 
+let s = "pwwkew"
+console.log(lengthOfLongestSubstring(s));
+console.log(lengthOfLongestSubstring2(s));
