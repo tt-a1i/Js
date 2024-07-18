730670dfc1b04979200eca959f6c1be4f328438e
rookie <2801884530@qq.com>
2024-07-18 23:33:45 +0800

    update

--- 例题/任务队列.js
+++ 例题/任务队列.js
@@ -1,20 +1,23 @@
 async function async1() {
-    console.log('async1 start')//2
+    console.log('async1 start')
     await async2()
-    console.log('async1 end')//6
+    console.log('async1 end')
 }
 async function async2() {
-    console.log('async2')//3
+    console.log('async2')
 }
-console.log('script start')//1
+console.log('script start')
 setTimeout(function () {
-    console.log('settimeout')//8
+    console.log('settimeout')
 })
 async1()
 new Promise(function (resolve) {
-    console.log('promise1')//4
+    console.log('promise1')
     resolve()
 }).then(function () {
-    console.log('promise2')//7
+    console.log('promise2')
 })
-console.log('script end')//5
+console.log('script end')
+
+
+// script start---async1 start---async2---promise1---script end---async1 end---promise2---settimeout
