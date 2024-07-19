297dbb2ddeca16cac10bb97499e356ec99e1b265
rookie <2801884530@qq.com>
2024-07-19 18:10:52 +0800

    update

--- 例题/demo.js
+++ 例题/demo.js
@@ -1,36 +1,36 @@
-function myPromiseAll(promises){
-    if(!promises[Symbol.iterator]){
-        throw new TypeError('Argument must be iterable')
+function myPromiseAll(promises) {
+  if (!promises[Symbol.iterator]) {
+    throw new TypeError("Argument must be iterable");
+  }
+  const promiseArr = Array.from(promises);
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let computed = 0;
+    if (promiseArr.length === 0) {
+      resolve(results);
+      return;
     }
-    const promiseAll = Array.from(promises)
-    return new Promise((resolve, reject) => {
-        const results = []
-        let computed = 0
-        if(promiseAll.length === 0){
-            resolve(results)
-            return
-        }
-        promiseAll.forEach((promise, index) => {
-            Promise.resolve(promise)
-                .then(value => {
-                    results[index] = value
-                    computed++
-                    if(computed === promiseAll.length){
-                        resolve(results)
-                    }
-                })
-                .catch(error => {
-                    reject(error)
-                })
+    promiseArr.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          results[index] = value;
+          computed++;
+          if (computed === promiseArr.length) {
+            resolve(results);
+            return;
+          }
         })
-    })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
 }
-const promises1 = [
+let promise1 = [
     Promise.resolve(1),
-    Promise.resolve(2),
-    Promise.resolve(3)
-  ];
-  
-  myPromiseAll(promises1)
-    .then(results => console.log('Test 1:', results))
-    .catch(error => console.error('Test 1 Error:', error));
+    Promise.reject(3)
+
+]
+myPromiseAll(promise1)
+    .then(resolve => console.log(resolve))
+    .catch(reject => console.log(reject))
