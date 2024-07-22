5b79a6f63f13d7444e1d3fa952a458870e3f38ba
名字 <2801884530@qq.com>
2024-07-22 13:09:12 +0800

    update

--- demo.js
+++ demo.js
@@ -1,22 +1,10 @@
-function myFlatten(arr, depth = Infinity){
-  const map = new WeakMap()
-  function flatten(arr, depth){
-    if(depth < 1) return arr.slice();
-    if(map.has(arr)) return map.get(arr);
-    const result = []
-    map.set(arr, result)
-    for(const item of arr){
-      if(Array.isArray(item)){
-        result.push(...flatten(item, depth - 1))
-      }else{
-        result.push(item)
-      }
+var uniqueInOrder=function(iterable){
+  //your code here - remember iterable can be a string or an array
+  if(!iterable) return []
+  return iterable.split('').reduce((prev, cur) => {
+    if(!prev.includes(cur)){
+      prev.push(cur)
     }
-    return result
-  }
-  return flatten(arr, depth)
+    return prev
+  }, [])
 }
-const arr = [1, 2, [3, 4]];
-arr[2].push(arr);
-
-console.log(myFlatten(arr));
