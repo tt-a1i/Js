6cb852d97bd457efbae5dfa78e262822e3a417fa
rookie <2801884530@qq.com>
2024-07-23 21:27:28 +0800

    1

--- 例题/输出例题/this对象指向例题.js
+++ 例题/输出例题/this对象指向例题.js
@@ -1,17 +1,17 @@
 var bar = 'window'
 function say(){
   var bar = '111'
-  console.log(bar);
-  console.log(this.bar);
+  console.log(bar);// '111' （局部变量 bar）
+  console.log(this.bar);// 'window' （this 指向全局对象，this.bar 即 window.bar）
 }
 const obj = {
   bar: '222',
   say(){
-    console.log(bar);
-    console.log(this.bar);
+    console.log(bar);// 没有声明 var bar，所以它会查找最近的上级作用域中的 bar，即全局作用域中的 bar
+    console.log(this.bar);// this 指向 obj 对象，this.bar 即 obj.bar
   }
 }
-say()
-obj.say()
+say()// 全局调用
+obj.say()// 作为对象的方法调用
 obj.say = say
-obj.say()
+obj.say()// 作为对象的方法调用，但函数体是 say
