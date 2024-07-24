365fa71cf46ff2c36a07d0205d574178bffdb466
rookie <2801884530@qq.com>
2024-07-24 23:04:54 +0800

    1

--- 例题/输出例题/立即执行函数的this.js
+++ 例题/输出例题/立即执行函数的this.js
@@ -7,9 +7,11 @@ const obj = {
       (function() {
         console.log(this.name);
         console.log(self.name);
-      })()
+      })()//bar  bar window bar undefined und und und
     }
-  }//立即执行函数的对象指向全局
+  }
+  // var name = 'window'
+  //立即执行函数的对象指向全局
   //普通函数内部的 this 并不指向函数内部。
   //this 的值取决于函数的调用方式，而不是函数的定义方式。
   //在大多数情况下（非严格模式下的全局调用），如果没有明确指定，this 会指向全局对象。
