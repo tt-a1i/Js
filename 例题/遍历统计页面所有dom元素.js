9c559a4669d530490a04572e1498b6e9693eaa0e
名字 <2801884530@qq.com>
2024-07-25 17:06:57 +0800

    1

--- 例题/遍历统计页面所有dom元素.js
+++ 例题/遍历统计页面所有dom元素.js
@@ -1,25 +1,18 @@
 function countDOMElements() {
-    // 初始化一个对象来存储元素计数
     const elementCounts = {};
 
-    // 递归函数来遍历 DOM 树
     function traverse(element) {
-        // 获取元素的标签名（转换为小写以保持一致性）
         const tagName = element.tagName.toLowerCase();
 
-        // 更新计数
         elementCounts[tagName] = (elementCounts[tagName] || 0) + 1;
 
-        // 遍历所有子元素
         for (let child of element.children) {
             traverse(child);
         }
     }
 
-    // 从 document.body 开始遍历
     traverse(document.body);
 
-    // 特殊处理：手动添加 <html> 和 <head> 元素的计数
     elementCounts['html'] = 1;
     elementCounts['head'] = 1;
 
@@ -28,3 +21,18 @@ function countDOMElements() {
 
 // 使用函数并打印结果
 console.log(countDOMElements());
+
+
+function func(elem){
+    const elems = {}
+    function travels(elem){
+      const tag = elem.tagName.toLowerCase()
+      elems[tag] = (elem[tag] || 0) + 1
+      for(let child of elem.children){
+        travels(child)
+      }
+    }
+    travels(document.body)
+    return elems;
+  }
+  console.log(func());
