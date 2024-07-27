0a1a76062b6726e7594b7eb9bddc0899735b7293
rookie <2801884530@qq.com>
2024-07-27 22:14:31 +0800

    1

--- demo.js
+++ demo.js
@@ -1,20 +1,8 @@
-function myFlatten(arr, depth = Infinity, map = new WeakMap()){
-    if(depth < 1) return arr.slice();
-    if(map.has(arr)) return map.get(arr);
-    const result = []
-    map.set(arr, result)
-    for(let item of arr){
-        if(Array.isArray(item)){
-            result.push(...myFlatten(item, depth - 1, map))
-        }else{
-            result.push(item)
-        }
-    }
-    return result
-}
-// 创建一个循环引用的数组
-const arr = [1, 2, [3, 4]];
-arr[2].push(arr);
+Object.prototype.a = 1
+Function.prototype.b = 2
+function test(){
 
-console.log(myFlatten(arr));
-// 输出: [1, 2, 3, 4]
+}
+var t = new test()
+console.log(t.a);
+console.log(t.b);
