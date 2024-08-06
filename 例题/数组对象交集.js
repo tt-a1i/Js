50514e557cd3e13988505b789745a80b63b85bcb
rookie <2801884530@qq.com>
2024-08-06 22:31:45 +0800

    8-6

---
+++ 例题/数组对象交集.js
@@ -0,0 +1,19 @@
+function findCommonElements(array1, array2){
+    const map = new Map(array1.map(item => [item.id, item]))
+    return array2.filter(item => map.has(item.id))
+}
+const array1 = [
+    { id: 11, name: 'Alice' },
+    { id: 21, name: 'Bob' },
+    { id: 31, name: 'Charlie' },
+    { id: 41, name: 'David' }
+  ];
+  
+  const array2 = [
+    { id: 21, age: 30 },
+    { id: 31, age: 25 },
+    { id: 51, age: 35 }
+  ];
+  
+  const result = findCommonElements(array1, array2);
+  console.log(result);
