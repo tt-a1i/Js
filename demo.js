aac697bd2a15ce6004a1bd585da23a00b48ce908
名字 <2801884530@qq.com>
2024-08-14 13:47:49 +0800

    update

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 function quickSort(arr, low = 0, high = arr.length - 1) {
-	while (low < high) {
-		let pivotIndex = partition(arr, low, high);
-		quickSort(arr, low, pivot - 1);
-		quickSort(arr, pivot + 1, high);
+	if (low < high) {
+		let pivotInd = partition(arr, low, high);
+		quickSort(arr, low, pivotInd - 1);
+		quickSort(arr, pivotInd + 1, high);
 	}
 	return arr;
 }
@@ -10,7 +10,7 @@ function partition(arr, low, high) {
 	const pivot = arr[high];
 	let i = low - 1;
 	for (let j = low; j < high; j++) {
-		if (arr[j] <= pivot) {
+		if (arr[j] >= pivot) {
 			i++;
 			[arr[i], arr[j]] = [arr[j], arr[i]];
 		}
@@ -18,3 +18,5 @@ function partition(arr, low, high) {
 	[arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
 	return i + 1;
 }
+console.log(quickSort([1,3,4,6,3,2]));
+
