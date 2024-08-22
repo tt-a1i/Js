0f2b2eb16be718a980c4af9c6f19298cc8381f52
名字 <2801884530@qq.com>
2024-08-22 13:47:06 +0800

    update

--- demo.js
+++ demo.js
@@ -1,14 +1,2 @@
-var obj1 = {
-	name: "obj1",
-	fn: function () {
-		console.log(this.name);
-	},
-};
-var obj2 = { name: "obj2" };
-var obj3 = { name: "obj3" };
-obj1.fn();//
-var newFn = obj1.fn;
-newFn();
-newFn.call(obj2);
-obj3.fn = newFn;
-obj3.fn();
+let str = 'abc345efgabcab';
+console.log(str.replace(/[a,b,c]/g, ''));
