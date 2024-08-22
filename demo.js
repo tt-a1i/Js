4c7026bc506c16dc37d9e961e91e06de6b19b1a5
rookie <2801884530@qq.com>
2024-08-22 23:28:13 +0800

    update

--- demo.js
+++ demo.js
@@ -1,7 +1,16 @@
 function myNew(constructor, ...args) {
+  //声名一个空对象
   const obj = {};
-  obj.__proto__ = constructor.prototype;
-
-  const result = constructor.apply(obj, args);
-   return 
+  //设置新对象的原型链,继承构造函数对象的属性和方法
+  Object.setPrototypeOf(obj, constructor.prototype)
+  //绑定this并且执行函数
+  let result = constructor.apply(obj, args)
+  //判断是否产生了新对象,如果构造函数返回一个非对象的值,则返回obj对象
+  return result instanceof Object ? result : obj
 }
+function Person(name){
+  this.name = name;
+}
+const person = myNew(Person, 'tom')
+console.log(person.name);
+console.log(Person.constructor === Person);
