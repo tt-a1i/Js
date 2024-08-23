b23833dfaa9c04cc3e32f862c417c765e7c25e40
名字 <2801884530@qq.com>
2024-08-23 09:42:15 +0800

    new

--- 例题/js/手写new.js
+++ 例题/js/手写new.js
@@ -3,10 +3,11 @@ function myNew(constructor, ...args) {
     // 1. 创建一个新的空对象
     const obj = {};
 
-    // 2. 将这个空对象的原型指向构造函数的 prototype 属性
+    // 2. 新对象的原型链接到构造函数的 prototype,确保实例能够访问到构造函数原型上的属性和方法
     Object.setPrototypeOf(obj, constructor.prototype);
 
-    // 3. 将构造函数的 this 绑定到新创建的对象上，并执行构造函数中的代码
+    // 3. 将构造函数的 this 绑定到新创建的对象上，确保了构造函数中的 this 指向以 new 方式调用时创建的对象，
+    //    并且构造函数内部定义的属性和方法会被正确地添加到这个对象上
     const result = constructor.apply(obj, args);
 
     // 4. 如果 result 不是对象（即为基本类型，如字符串、数字等），则返回新创建的对象 obj
