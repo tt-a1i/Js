1ec280ed58ebc49e2a250c5f57019c7c22349d1e
名字 <2801884530@qq.com>
2024-08-23 18:36:28 +0800

    update

---
+++ 例题/js/大数相加.js
@@ -0,0 +1,27 @@
+function addLargeNumbers(num1, num2) {
+    // 确保 num1 是较长的数
+    if (num1.length < num2.length) {
+        [num1, num2] = [num2, num1];
+    }
+
+    let carry = 0;
+    let result = '';
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const digit1 = i >= 0 ? parseInt(num1[i]) : 0;
+        const digit2 = j >= 0 ? parseInt(num2[j]) : 0;
+
+        const sum = digit1 + digit2 + carry;
+        result = (sum % 10) + result;
+        carry = Math.floor(sum / 10);
+
+        i--;
+        j--;
+    }
+
+    return result;
+}
+console.log(addLargeNumbers('123456789', '987654321')); // 输出: "1111111110"
+console.log(addLargeNumbers('9007199254740991', '1')); // 输出: "9007199254740992"
