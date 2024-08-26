be8307a9121a90697e0d9a42a27e5dcce9b56515
名字 <2801884530@qq.com>
2024-08-26 19:10:47 +0800

    update

--- 例题/算法例题/快手-分红包.js
+++ 例题/算法例题/快手-分红包.js
@@ -9,11 +9,15 @@ function splitRedEnvelope(totalAmount, numEnvelopes, minAmount, maxAmount) {
 	let remainingAmount = totalAmount;
 	let remainingEnvelopes = numEnvelopes;
 	for (let i = 0; i < numEnvelopes - 1; i++) {
+        //remainingAmount - minAmount * (remainingEnvelopes - 1)保证此次分配后,剩余金额满足剩下未分配的最小金额要求
 		let maxPossible = Math.min(
 			maxAmount,
 			remainingAmount - minAmount * (remainingEnvelopes - 1)
 		);
 		let minPossible = minAmount;
+        //红包范围平移到[minPossible, maxPossible]
+        //乘以 Math.random() 得到一个在 [0, maxPossible - minPossible + 1) 范围内的浮点数
+        //Math.floor()向下取整，得到 [0, maxPossible - minPossible] 范围内的一个整数
 		let envelope =
 			Math.floor(Math.random() * (maxPossible - minPossible + 1)) + minPossible;
 		envelopes.push(envelope);
@@ -23,6 +27,7 @@ function splitRedEnvelope(totalAmount, numEnvelopes, minAmount, maxAmount) {
 	envelopes.push(remainingAmount);
 	return envelopes;
 }
+
 try {
     let result = splitRedEnvelope(100, 5, 10, 30);
     console.log('红包金额集合:', result);
