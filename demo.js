d1a3acee2038714a0bfef8148e11e82668c97918
名字 <2801884530@qq.com>
2024-08-27 13:38:58 +0800

    update

--- demo.js
+++ demo.js
@@ -1,32 +1,20 @@
-function splitRedEnvelop(totalAmount, numEnvelopes, minAmount, maxAmount) {
-	if (
-		minAmount * numEnvelopes > totalAmount ||
-		maxAmount * numEnvelopes < totalAmount
-	) {
-		throw new Error("无法分配");
-	}
-	let envelopes = [];
-	let remainingAmount = totalAmount;
-	let remainingEnvelopes = numEnvelopes;
-	for (let i = 0; i < numEnvelopes - 1; i++) {
-		let maxPossible = Math.min(
-			maxAmount,
-			remainingAmount - minAmount * (numEnvelopes - 1)
-		);
-		let minPossible = minAmount;
-		let envelope = Math.floor(
-			Math.random() * (maxPossible - minPossible + 1) + minPossible
-		);
-		envelopes.push(envelope);
-		remainingAmount -= envelope;
-		remainingEnvelopes--;
-	}
-	envelopes.push(remainingAmount);
-	return envelopes;
-}
-try {
-	let result = splitRedEnvelop(100, 5, 30, 30);
-	console.log(result);
-} catch (e) {
-	console.log(e.message);
-}
+var name = "window";
+var obj = {
+	name: "obj",
+	say() {
+		console.log(this.name);
+	},
+	say1: () => {
+		console.log(this.name);
+	},
+};
+obj.say();
+obj.say1();
+var fn = obj.say;
+var fn1 = obj.say1;
+fn();
+fn1();
+//obj 
+//undefined
+//undifined
+//undefined
