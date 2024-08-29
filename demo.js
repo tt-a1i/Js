e55a2e515718b65bec53cad9f17b406fc0b0be3c
名字 <2801884530@qq.com>
2024-08-29 11:07:57 +0800

    blob

--- demo.js
+++ demo.js
@@ -1,6 +1,26 @@
-function func(arr, pos = arr.length - 1){
-	if(pos < 0) return;
-	console.log(arr[pos]);
-	func(arr, pos - 1)
+class EventEmitter{
+	constructor(){
+		this.events = {}
+	}
+	on(event, listener){
+		if(!this.events[event]){
+			this.events[event] = []
+		}
+		this.events[event].push(listener)
+	}
+	destroy(event, listener){
+		if(!this.events[event]) return;
+		this.events[event] = this.events[event].filter(l => l !== listener)
+	}
+	emit(event, ...args){
+		if(!this.events[event]) return;
+		this.events[event].forEach(l => l(...args))
+	}
 }
-func([1,2,3,4,5])
+const emitter1 = new EventEmitter()
+const greet = name => console.log(name);
+emitter1.on('greet', greet)
+emitter1.emit('greet', 'tiger')
+emitter1.destroy('greet', greet)
+emitter1.emit('greet', 'tiger')
+
