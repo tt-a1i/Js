4a80664dd8a59f88e6b9abc012d209ed81fa3ba6
名字 <2801884530@qq.com>
2024-08-30 20:30:45 +0800

    洗牌算法

--- demo.js
+++ demo.js
@@ -1,8 +1,13 @@
-function log() {
-	console.log('a')
+function shuffleArray(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		let n = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[n]] = [
+			arr[n],
+			arr[i],
+		];
+	}
 }
-const debounceFn = debounce(log, 350)//防抖函数
-
-for(let i = 1; i <= 10; i++){
-	setTimeout(debounceFn, i * 100)
-}
+// 使用示例
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("Original Array:", arr);
+console.log("Shuffled Array:", shuffleArray(arr));
