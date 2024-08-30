a8d05e93e1ae39ad3d0bc15d023bf87db07ddd7d
名字 <2801884530@qq.com>
2024-08-30 18:55:25 +0800

    函数声明和变量声明

--- demo.js
+++ demo.js
@@ -1,17 +1,8 @@
-function Func() {
-	getName = () => console.log(1)
-	return this;
-}
-Func.getName = () => console.log(2);
-Func.prototype.getName = () => console.log(3);
-
+getName()
 var getName = () => console.log(4);
 function getName() {
 	console.log(5);
 }
-Func.getName()
 getName()
-Func().getName()
-getName()
-//3 4 4 4
+
 
