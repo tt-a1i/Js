57ff1fca5274b4a5a182f688c68ccc8800bc3124
名字 <2801884530@qq.com>
2024-09-06 13:57:56 +0800

    null不指向堆栈, 是一个特殊的标记

--- demo.js
+++ demo.js
@@ -1,15 +1,11 @@
-// 定义构造函数
-function MyObject() {
-    // 在构造函数中定义数组属性
-    this.myArray = [];
-  }
-  
-  // 创建两个实例
-  let obj1 = new MyObject();
-  let obj2 = new MyObject();
-  
-  // 修改第一个实例的数组
-  obj1.myArray.push('item1');
-  
-  console.log('obj1.myArray:', obj1.myArray);  // 输出: ['item1']
-  console.log('obj2.myArray:', obj2.myArray);  // 输出: []
+class Animal{
+    constructor(){
+        this.height = 100
+    }
+}
+let a = new Animal()
+let b = new Animal()
+a.height = 10
+console.log(a.height);
+console.log(b.height);
+
