1b54cb38f458fdcb57423121f287acec5c77b48a
rookie <2801884530@qq.com>
2024-09-11 22:54:14 +0800

    提问

--- demo.js
+++ demo.js
@@ -1,24 +1,4 @@
-function longestPalindrome(s) {
-	let start = 0;
-	let maxLength = 1;
-	function expandAroundCenter(left, right) {
-		while (left >= 0 && right < s.length && s[left] === s[right]) {
-			const currentLen = right - left + 1;
-			if (currentLen > maxLength) {
-				start = left;
-				maxLength = currentLen;
-			}
-			left--;
-			right++;
-		}
-	}
-	for (let i = 0; i < s.length; i++) {
-    expandAroundCenter(i, i)
-    expandAroundCenter(i, i + 1)
-  }
-  return s.substring(start, start + maxLength)
+function test(){
+  var a = 4
 }
-console.log(longestPalindrome("babad")); // 输出 "bab" 或 "aba"
-console.log(longestPalindrome("cbbd"));  // 输出 "bb"
-console.log(longestPalindrome("a"));     // 输出 "a"
-console.log(longestPalindrome("ac"));    // 输出 "a"
+console.log(a);
