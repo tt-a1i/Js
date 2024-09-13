489e283320fcccb599fab5f0ca25da6d6c450133
rookie <2801884530@qq.com>
2024-09-13 18:15:03 +0800

    立即函数表达式独立作用域

--- demo.js
+++ demo.js
@@ -1,15 +1,9 @@
-const a = {}
-
-const b = {'key':123}
-
-const c = {'key':456}
-
-const d = {'key':789}
-
-a[b] = 123
-
-a[c] = 456
-
-a[d] = 789
-
-console.log(a[b]);
+//通过立即执行函数表达式（IIFE）创建一个新的作用域，
+//将每次循环的 i 值传递给 IIFE，从而在每次迭代中捕获不同的 i 值。
+for (var i = 0; i < 5; i++) {
+  (function(i) {
+    setTimeout(() => {
+      console.log(i);
+    }, 0);
+  })(i);
+}
