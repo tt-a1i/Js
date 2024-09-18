360fd787611b0da4ef259bd7579e8f4cdc2dda4d
rookie <2801884530@qq.com>
2024-09-18 08:52:45 +0800

    目录调整

--- demo.js
+++ demo.js
@@ -1,8 +1,21 @@
-function A(){
-	this.a = []
+function sumPairs(ints, s) {
+	const map = new Map();
+	let m = null,
+		n = null;
+	for (let i = 0; i < ints.length; i++) {
+		if (map.has(s - ints[i])) {
+			if (!m && !n) {
+				m = s - ints[i];
+				n = ints[i];
+			}
+			else if (i < map.get(n)) {
+				m = s - ints[i];
+				n = ints[i];
+			}
+		}
+		map.set(ints[i], i);
+	}
+	return !m ? undefined : [m, n];
 }
-let b = new A()
-let c = new A()
-console.log(b.a);
-b.a.push(1)
-console.log(c.a);
+let ints = [1, -2, 3, 0, -6, 1];
+console.log(sumPairs(ints, -6));
