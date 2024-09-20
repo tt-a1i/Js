9846202b14b996b89cb5a032f8ebaf42b8019563
rookie <2801884530@qq.com>
2024-09-20 11:22:15 +0800

    splice返回值,一个包含了删除了元素的数组,第3个参数为在第一个参数值的索引处插入的值

--- demo.js
+++ demo.js
@@ -1,24 +1,3 @@
-function longestPalindrome(s) {
-	if (s.length < 2) return s;
-	let maxLen = 1,
-		start = 0;
-	function expandAroundCeter(left, right) {
-		while (left >= 0 && right < s.length && s[left] === s[right]) {
-			if (right - left + 1 > maxLen) {
-				maxLen = right - left + 1;
-				start = left;
-			}
-			left--;
-			right++;
-		}
-	}
-	for (let i = 0; i < s.length; i++) {
-		expandAroundCeter(i, i);
-		expandAroundCeter(i, i + 1);
-	}
-	return s.substring(start, start + maxLen)
-}
-console.log(longestPalindrome("babad")); // 输出 "bab" 或 "aba"
-console.log(longestPalindrome("cbbd")); // 输出 "bb"
-console.log(longestPalindrome("a")); // 输出 "a"
-console.log(longestPalindrome("ac")); // 输出 "a"
+const a = [0, 1]
+console.log(a.splice(1,0,2))
+console.log(a)
