76b44b98dde2448fe89acbfd41e39963f21f2ce9
rookie <2801884530@qq.com>
2024-09-23 10:55:55 +0800

    csp, content-security或meta实现,指定内容的来源

--- demo.js
+++ demo.js
@@ -1,6 +1,11 @@
-function myNew(constructor, ...args){
-  const obj = {}
-  Object.setPrototypeOf(obj, constructor.prototype)
-  const result = constructor.apply(obj, args)
-  return result instanceof Object ? result : obj
+function myNew(constructor, ...args) {
+	const obj = {};
+	Object.setPrototypeOf(obj, constructor.prototype);
+	const result = constructor.apply(obj, args);
+	return result instanceof Object ? result : obj;
 }
+function Animal(name) {
+	this.name = name;
+}
+let tiger = myNew(Animal, "tiger");
+console.log(tiger.name);
