defc362ba0a8c93d695f33c1f261339d12f0bdcd
rookie <2801884530@qq.com>
2024-10-05 20:35:13 +0800

    手写响应式实现

---
+++ code_sample/js/响应式实现/index.js
@@ -0,0 +1,32 @@
+let activeEffect = null
+export function effect(fn){
+    activeEffect = fn
+    fn()
+    activeEffect = null
+}
+
+export function reactive(obj){
+    const effectMap = {}
+    return new Proxy(obj, {
+        get(target, key){
+            if(activeEffect){
+                if(!effectMap[key]){
+                    effectMap[key] = []
+                }
+                effectMap[key].push(activeEffect)
+            }
+            console.log('收集依赖', key, activeEffect);
+            return target[key]
+        },
+        set(target, key, val){
+            console.log('派发更新');
+            target[key] = val
+            effectMap[key] ?.forEach(fn => fn());
+            return true
+        }
+    })
+}
+
+export function ref(value){
+    return reactive({value: value})
+}
