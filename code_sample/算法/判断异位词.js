bf01313a3ebf3f6be09110dd4ef78211c1ae44b6
rookie <2801884530@qq.com>
2024-10-09 21:34:19 +0800

    判断易位词

--- code_sample/算法/判断异位词.js
+++ code_sample/算法/判断异位词.js
@@ -1,16 +1,15 @@
-function isAnagram(s1, s2){
-    if(s1.length !== s2.length) return false
-    let arr = new Array(100).fill(0)
-    for(let c of s1){
-        arr[c.charCodeAt() - 65]++
+function isAnagram(s1, s2) {
+    if (s1.length !== s2.length) return false;
+    let arr = new Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+        arr[s1.charCodeAt(i) - 97]++;
+        arr[s2.charCodeAt(i) - 97]--;
     }
-    for(let c of s2){
-        arr[c.charCodeAt() - 65]--
-    }
-    for(let n of arr){
-        if(n !== 0) return false
+    for (let n of arr) {
+        if (n !== 0) return false;
     }
     return true;
 }
+
 console.log(isAnagram('listen', 'silent')); // true
 console.log(isAnagram('hello', 'world'));   // false
