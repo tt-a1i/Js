a22870ee9a3201907797287a4da2d843e8761ea3
rookie <2801884530@qq.com>
2024-10-14 21:41:19 +0800

    review更新

--- demo.js
+++ demo.js
@@ -1,7 +1,15 @@
-function flatReduce(arr){
-	return arr.reduce((prev, cur) => {
-		return Array.isArray(cur) ? prev.concat(flatReduce(cur)) : prev.concat(cur)
-	},[])
+function Test(){
+
 }
-let arr = [1,[2,[3]]]
-console.log(flatReduce(arr))
+Test.prototype.fn = function(){
+    console.log(1);
+}
+let test = new Test()
+test.fn()
+
+test.fn = function(){
+    console.log(2)
+}
+test.fn()
+
+console.log(test.prototype = {});
