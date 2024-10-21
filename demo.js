8c73e63eb479c2d4ac90a9213c64efb6ef5c4a8b
rookie <2801884530@qq.com>
2024-10-21 22:16:32 +0800

    10-21

--- demo.js
+++ demo.js
@@ -1,14 +1,1 @@
-window.addEventListener('click', () => {
-	Promise.resolve().then(() => {
-		setTimeout(() => {
-			console.log(1);
-		})
-	})
-})
-window.addEventListener('click', () => {
-	setTimeout(() => {
-		Promise.resolve().then(() => {
-			console.log(2)
-		})
-	})
-})
+
