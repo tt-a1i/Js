3f9d07a891040d3e55df25023aa2f0d6548a6c08
rookie <2801884530@qq.com>
2024-10-23 22:49:02 +0800

    lodash的get方法

---
+++ code_sample/js/lodash的get方法.js
@@ -0,0 +1,19 @@
+function get(object, path, defaultValue) {
+	//路径转换为数组
+	const pathArray = Array.isArray(path)
+		? path
+		: path.replace(/\[(\d+)\]/g, ".$1").split(".");
+	let result = object;
+	for (const key of pathArray) {
+		result = result ? result[key] : undefined;
+		if (result === undefined) return defaultValue;
+	}
+	return result;
+}
+
+const obj = { a: { b: { c: 42 } } };
+
+console.log(get(obj, 'a.b.c')); // 输出: 42
+console.log(get(obj, 'a.b.d', 'default')); // 输出: 'default'
+console.log(get(obj, ['a', 'b', 'c'])); // 输出: 42
+console.log(get(obj, 'a.b.c.d', 'default')); // 输出: 'default'
--- demo.js
+++ demo.js
@@ -1,12 +1,1 @@
-Function.prototype.myApply = function(ctx, args){
-    ctx = ctx || globalThis
-    const f = Symbol()
-    ctx[f] = this
-    const res = ctx[f](...(args || []))
-    delete ctx[f]
-    return res
-}
-function say(a, b){
-    console.log(this.name + ` ${a} + ${b}`)
-}
-say.myApply({name: 'tom'}, [1,2])
+console.log('a[0][1].b'.replace(/\[(\d+)\]/g, ".$1"))
