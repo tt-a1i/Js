a07487aa670bff1c7af3400d38cfdcc7730a01bf
rookie <2801884530@qq.com>
2024-10-29 22:04:44 +0800

    10-29

--- demo.js
+++ demo.js
@@ -1,14 +1,12 @@
-function demoPromiseRace() {
-    const promise2 = new Promise((_, reject) => {
-        reject("Rejected after 100ms");
-    });
-    const promise1 = new Promise((resolve) => {
-        resolve("Resolved after 100ms");
-    });
-
-
-    promise2.catch((err) => console.log(err));
-    promise1.then((res) => console.log(res)).catch((err) => console.log(err));
+async function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-demoPromiseRace();
+async function processArray() {
+	for (const n of [1, 2, 3, 4, 5]) {
+		await sleep(1000);
+		console.log(n);
+	}
+}
+
+processArray();
