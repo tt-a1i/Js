a5ab349753fbd1e5906b0bd87ff65b092acd097c
rookie <2801884530@qq.com>
2024-11-04 22:04:47 +0800

    并发请求限制优化

--- demo.js
+++ demo.js
@@ -1,6 +1,36 @@
-Object.prototype.a = 1;
-Function.prototype.b = 2;
-function test() {}
-var t = new test();
-console.log(t.a);//1
-console.log(t.b);//2
+function limitConcurrency(requests, limit){
+	let activeCount = 0
+	let currIdx = 0
+	const results = []
+	return new Promise(resolve => {
+		function next(){
+			if(activeCount === 0 && currIdx === requests.length){
+				resolve(results)
+			}
+			while(activeCount < limit && currIdx < requests.length){
+				activeCount++
+				const request = requests[currIdx++]
+				request()
+					.then(res => results.push(res))
+					.catch(err => results.push(err))
+					.finally(() => {
+						activeCount--
+						next()
+					})
+			}
+		}
+		next()
+	})
+}
+
+const request = (i) => () => new Promise(resolve => {
+	console.log(`Request ${i} started`)
+	setTimeout(() => {
+		console.log(`Request ${i} completed`)
+		resolve(`Request ${i} completed`)
+	}, Math.random() * 1000);
+})
+
+const requests = Array.from({length: 10}, (_, i) => request(i))
+limitConcurrency(requests, 2)
+	.then(res => console.log(`Requests all completed`, res))
