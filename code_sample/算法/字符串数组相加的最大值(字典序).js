bac07330bd3f2f881d09e65d0b1ff6b40e75a01c
rookie <2801884530@qq.com>
2024-11-05 15:55:23 +0800

    字典序

---
+++ code_sample/算法/字符串数组相加的最大值(字典序).js
@@ -0,0 +1,10 @@
+function largestNumber(str){
+    const strNums = str.map(String)
+    strNums.sort((a, b) => (b + a).localeCompare(a + b))
+    console.log(strNums)
+    if(strNums[0] == 0) return '0'
+    return strNums.join('')
+}
+let nums = [3, 30, 34, 5, 9];
+console.log(largestNumber(nums)); // 输出 "9534330"
+console.log(largestNumber([0,0,0]))
--- demo.js
+++
@@ -1,9 +0,0 @@
-const p = new Promise((reject) => {
-	reject(1);
-})
-	.catch((err) => console.log(err))
-	.then(
-		() => console.log(2),
-		() => console.log(3)
-	);
-console.log(p);
