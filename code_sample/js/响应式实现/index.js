65becec1fb318ffd800be0059e1ac92cd641e860
rookie <2801884530@qq.com>
2024-11-07 22:05:55 +0800

    11-7

--- code_sample/js/响应式实现/index.js
+++ code_sample/js/响应式实现/index.js
@@ -1,33 +1,31 @@
-let activeEffect = null
-export function effect(fn){
-    //在执行fn更新页面数据时,会触发proxy的get,进而对依赖进行收集
-    activeEffect = fn
-    fn()
-    activeEffect = null
+let activeEffect = null;
+export function effect(fn) {
+	//在执行fn更新页面数据时,会触发proxy的get,进而对依赖进行收集
+	activeEffect = fn;
+	fn();
+	activeEffect = null;
 }
 
-export function reactive(obj){
-    const effectMap = {}
-    return new Proxy(obj, {
-        get(target, key){
-            if(activeEffect){
-                if(!effectMap[key]){
-                    effectMap[key] = []
-                }
-                effectMap[key].push(activeEffect)
-            }
-            console.log('收集依赖', key, activeEffect);
-            return target[key]
-        },
-        set(target, key, val){
-            console.log('派发更新');
-            target[key] = val
-            effectMap[key] ?.forEach(fn => fn());
-            return true
-        }
-    })
+export function reactive(obj) {
+	const effectMap = {};
+	return new Proxy(obj, {
+		get(target, key) {
+			if (activeEffect) {
+				effectMap[key] ??= [];
+				effectMap[key].push(activeEffect);
+			}
+			console.log("收集依赖", "\n", target, "\n", key, "\n", activeEffect);
+			return target[key];
+		},
+		set(target, key, val) {
+			console.log("派发更新");
+			target[key] = val;
+			effectMap[key]?.forEach((fn) => fn());
+			return true;
+		},
+	});
 }
 
-export function ref(value){
-    return reactive({value: value})
+export function ref(value) {
+	return reactive({ value: value });
 }
--- demo.js
+++ demo.js
@@ -1,26 +1,11 @@
-function longestPalindrome(s) {
-    let start = 0, maxLen = 0
 
-    function expandAroundCenter(l, r) {
-        while (l >= 0 && r < s.length && s[l] === s[r]) {
-            const curLen = r - l + 1
-            if (curLen > maxLen) {
-                start = l
-                maxLen = curLen
-            }
-            l--
-            r++
-        }
-    }
-
-    for (let i = 0; i < s.length; i++) {
-        expandAroundCenter(i, i)
-        expandAroundCenter(i, i + 1)
-    }
-    return s.substring(start, start + maxLen)
-}
-
-console.log(longestPalindrome("babad")); // 输出 "bab" 或 "aba"
-console.log(longestPalindrome("cbbd")); // 输出 "bb"
-console.log(longestPalindrome("a")); // 输出 "a"
-console.log(longestPalindrome("ac")); // 输出 "a"
+setTimeout(() => {
+	console.log(3);
+}, 0);
+Promise.resolve().then(() => {
+	console.log(1);
+	Promise.resolve().then(() => {
+		console.log(2);
+	});
+});
+console.log(4)
