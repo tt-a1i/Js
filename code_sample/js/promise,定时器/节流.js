e7b373b6b02b4a3c2947088466f03efed2a86a92
rookie <2801884530@qq.com>
2024-11-09 13:01:20 +0800

    节流函数

--- code_sample/js/promise,定时器/节流.js
+++ code_sample/js/promise,定时器/节流.js
@@ -1,16 +1,33 @@
-function throttle(fn, delay){
-    let timer = null
-    return function(...args){
-        if(!timer) {
-            fn.apply(this, args)
-            timer = setTimeout(() => {
-                timer = null
-            }, delay)
-        }
-    }
+// 第一个版本（保存最后一次调用）
+function throttle1(fn, del) {
+	var timeout = null;
+	var lastArgs = null;
+	return function (...args) {
+		if (!timeout) {
+			fn(...args);
+			timeout = setTimeout(() => {
+				lastArgs && fn(...lastArgs);
+				lastArgs = null;
+				timeout = null;
+			}, del);
+		} else lastArgs = args;
+	};
+}
+
+// 第二个版本（丢弃中间调用）
+function throttle2(fn, delay) {
+	let timer = null;
+	return function (...args) {
+		if (!timer) {
+			fn.apply(this, args);
+			timer = setTimeout(() => {
+				timer = null;
+			}, delay);
+		}
+	};
 }
 let test = () => console.log(1);
-let fn = throttle(test, 3000)
+let fn = throttle2(test, 3000);
 fn(); // 应该会立即输出 1
 setTimeout(fn, 1000); // 不会输出，因为在 3000ms 内
 setTimeout(fn, 4000); // 应该会输出 1，因为超过了 3000ms
