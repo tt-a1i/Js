257515e57fdbd6a6e9fc28275357ad5354a457f7
rookie <2801884530@qq.com>
2024-11-12 13:44:03 +0800

    多数组交集

--- demo.js
+++ demo.js
@@ -1,12 +1,17 @@
-function intersect(...arrays) {
-	return arrays.reduce((acc, arr) => {
-		return acc.filter((item) => arr.includes(item));
-	});
+function intersect(...arr){
+	const sets = arr.map(item => new Set(item))
+	const result = sets[0]
+	for(let i = 1; i < sets.length; i++){
+		result.forEach(item => {
+			if(!sets[i].has(item)){
+				result.delete(item)
+			}
+		})
+	}
+	return Array.from(result)
 }
-
-// 示例用法
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [4, 5, 6, 7, 8];
 const array3 = [5, 6, 7, 8, 9];
 
-console.log(intersect(array1, array2, array3)); // 输出: [5]
+console.log(intersect(array1, array2, array3))
--- 知识点总结/leetcode/hot100/test.js
+++
@@ -1,6 +0,0 @@
-name = 'a'
-let obj = {
-    name: 'b',
-    func: () => console.log(this.name)
-}
-obj.func()
