f5b882ed8ea33c9ed30588af59b9c25db6a5b407
rookie <2801884530@qq.com>
2024-11-14 16:23:34 +0800

    comment

--- demo.js
+++ demo.js
@@ -1,14 +1,14 @@
 function deleteNth(arr, n) {
-	const map = new Map();
-	return arr.filter((num, index) => {
-		if (map.has(num)) {
-			if (map.get(num) === n) return false;
-            map.set(num, (map.get(num) || 0) + 1);
-			return true;
-		} else {
-			map.set(num, (map.get(num) || 0) + 1);
-			return true;
-		}
-	});
+    const map = new Map();
+    return arr.filter((num) => {
+        const count = map.get(num) || 0;
+        if (count >= n) {
+            return false;
+        }
+        map.set(num, count + 1);
+        return true;
+    });
 }
-console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3))
+
+// 测试
+console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)); //  [1, 1, 3, 3, 7, 2, 2, 2]
