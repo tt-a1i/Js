3f12c00057f0047ee7b1c9bfe54d48bd115f631d
rookie <2801884530@qq.com>
2024-12-14 22:37:01 +0800

    12-14

--- demo.js
+++ demo.js
@@ -1,10 +1,21 @@
-function render(template, data) {
-    return template.replace(/{\s*([^}]+)\s*}/g, (match, key) => {
-        const trimmedKey = key.trim();
-        console.log('Captured key:', trimmedKey); // 调试用
-        return trimmedKey in data ? data[trimmedKey] : match;
-    });
+//在有序数组中查找目标值第一次出现的位置，没有则返回-1
+function findTarget(arr, n){
+    let idx = null
+    let l = 0, r = arr.length - 1
+    while(l <= r){
+        let middle = Math.floor((l + r) / 2)
+        let num = arr[middle]
+        if(num === n){
+            while(arr[middle] === arr[middle - 1]) middle--
+            idx = middle
+            break
+        }else if(num < n){
+            l = middle + 1
+        }else{
+            r = middle - 1
+        }
+    }
+    return idx === null ? -1 : idx
 }
-
-const result = render('Welcome to { companyName }', { companyName: 'FutureGene' });
-console.log(result); // 应输出: Welcome to FutureGene
+let arr = [1,2,3,3,3,3,3,4]
+console.log(findTarget(arr, 3))
